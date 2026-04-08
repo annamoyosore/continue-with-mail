4e8ea641b7c1e6baa7829ddd0bc3ddc82202cb6f
annamoyosore <annamoyosore@gmail.com>
2026-04-08 13:25:05 +0100

    Update googleAuth.js

--- src/utils/googleAuth.js
+++ src/utils/googleAuth.js
@@ -1,7 +1,7 @@
 // utils/googleAuth.js
 export function getAuthUrl() {
-  const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-  const REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
+  const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+  const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 
   return `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
     REDIRECT_URI
