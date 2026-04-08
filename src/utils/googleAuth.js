a5e1b1961bb7c2dbd3fe0d85035bb03038df5b1a
annamoyosore <annamoyosore@gmail.com>
2026-04-08 08:45:29 +0100

    Update googleAuth.js

--- src/utils/googleAuth.js
+++ src/utils/googleAuth.js
@@ -1,15 +1,9 @@
-import { google } from "googleapis";
-
+// utils/googleAuth.js
 export function getAuthUrl() {
-  const oAuth2Client = new google.auth.OAuth2(
-    process.env.REACT_APP_GOOGLE_CLIENT_ID,
-    process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
-    process.env.REACT_APP_GOOGLE_REDIRECT_URI
-  );
+  const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
 
-  return oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: ["https://www.googleapis.com/auth/gmail.readonly"],
-    prompt: "consent"
-  });
+  return `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+    REDIRECT_URI
+  )}&response_type=code&scope=https://www.googleapis.com/auth/gmail.readonly&access_type=offline&prompt=consent`;
 }
